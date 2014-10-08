73e2c4c8fa7c13f529b70f4c9dae2d173c419404
harrylclc <harryliang910708@gmail.com>
2014-10-08 17:41:16 -0400

    proj2

---
+++ proj2/svm_1.py
@@ -0,0 +1,30 @@
+from sklearn import svm
+from util import load_data
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn import cross_validation
+import numpy as np
+n_fold = 5
+
+x, y = load_data(k=2)
+kf = cross_validation.KFold(len(x), n_fold)
+acc, prec, recall = [], [], []
+clf = svm.SVC(kernel='linear')
+
+for train, test in kf:
+    print 'iter {}'.format(len(acc))
+    x_train, x_test, y_train, y_test = x[train] , x[test] , y[train] , y[test]
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    acc.append(accuracy_score(y_test, y_pred))
+    prec.append(precision_score(y_test, y_pred))
+    recall.append(recall_score(y_test, y_pred))
+    print acc
+a = np.mean(acc)
+p = np.mean(prec)
+r = np.mean(recall)
+f = 2 * p * r / (p + r)
+
+print 'precision: {}'.format(p)
+print "recall: {}".format(r)
+print "f1: {}".format(f)
+print "accuracy: {}".format(a)
---
+++ proj2/util.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_iris, make_classification, load_svmlight_file
+def load_data(k=0):
+    print 'loading'
+    if k == 0:
+        iris = load_iris()
+        x = iris.data
+        y = iris.target
+    elif k == 1:
+        toy_data = make_classification(n_samples=500, n_features=14)
+        x = toy_data[0]
+        y = toy_data[1]
+    elif k == 2:
+        fname = '../data/art_feature_4week'
+        x, y = load_svmlight_file(fname)
+        x = x.toarray()
+    print 'done'
+    return x, y
